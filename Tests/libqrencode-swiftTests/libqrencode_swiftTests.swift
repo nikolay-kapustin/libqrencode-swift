a1359a0a2024fed0464f56601aae901db0031620
Nikolay Kapustin <greenkaktus@gmail.com>
2020-06-24 00:02:50 +0300

    fix another for renaming

--- Tests/libqrencode-swiftTests/libqrencode_swiftTests.swift
+++ Tests/libqrencode-swiftTests/libqrencode_swiftTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import libqrencode_swfit
+@testable import libqrencode_swift
 
 final class libqrencode_swfitTests: XCTestCase {
 
     func testQR() {
         let str = "https://apple.com"
         let colors:[Int] = [120,200,45]
-        let result = QREncodeSwift.qr(from: str, level: .middle, outputFileName: "./qr.png", colorComponents:nil)
+        let result = QREncodeSwift.qr(from: str, level: .middle, outputFileName: "./qr.png", colorComponents:colors)
         XCTAssert(result)
     }
 
