8afc00bf3f08b3f029beda22a8e38ef0b036a08d
Nikolay Kapustin <greenkaktus@gmail.com>
2020-06-23 20:08:07 +0300

    fix tests

--- Tests/libqrencode-swfitTests/libqrencode_swfitTests.swift
+++ Tests/libqrencode-swfitTests/libqrencode_swfitTests.swift
@@ -6,7 +6,7 @@ final class libqrencode_swfitTests: XCTestCase {
     func testQR() {
         let str = "https://apple.com"
         let colors:[Int] = [120,200,45]
-        let result = QREncodeSwift.qr(from: str, level: .middle, outputFileName: "/Users/nkapustin/Downloads/qr.png", colorComponents:nil)
+        let result = QREncodeSwift.qr(from: str, level: .middle, outputFileName: "./qr.png", colorComponents:nil)
         XCTAssert(result)
     }
 
